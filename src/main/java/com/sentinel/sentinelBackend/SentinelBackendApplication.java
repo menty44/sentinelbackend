9e3d58c6b3d3ca92e038cbd7d807a2c123cf84b8
menty44 <menty44@gmail.com>
2019-02-15 16:17:12 +0300

    first commit

---
+++ src/main/java/com/sentinel/sentinelBackend/SentinelBackendApplication.java
@@ -0,0 +1,14 @@
+package com.sentinel.sentinelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sentinel/sentinelBackend/SentinelBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sentinel.sentinelBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SentinelBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
